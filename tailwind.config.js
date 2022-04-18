604841a9d6ef3c8422fd6f7d9eeecdc5fc37d694
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-04-19 03:56:59 +0400

    feat: add website

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
