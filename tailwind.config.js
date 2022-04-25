27be38f78df113b4b18d4ae85a9ad1898b2bbb34
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-04-25 17:43:33 +0400

    fix: add layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["docs/**/*.html"],
+  content: ["docs/**/*.html", "layouts/**/*.html",],
   theme: {
     extend: {
       fontFamily: {
