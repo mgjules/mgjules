7c4433efba05c189662132cbbe5b01282598de1b
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-04-25 17:38:39 +0400

    chore: change src to css, purge css from generated html instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["layouts/**/*.html"],
+  content: ["docs/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
