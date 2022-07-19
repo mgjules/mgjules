07db670f2977084980918012186533221031be89
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-07-19 11:17:08 +0400

    add `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  experimental: { integrations: true },
+  integrations: [tailwind(), compress()]
 });
