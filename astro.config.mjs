99a3ffa3e840a72b19605ba0d981e337799a67a2
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-07-21 14:58:59 +0400

    move to `uno.css`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,44 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
+import unocss from "@unocss/vite"
+import { presetAttributify, presetUno, presetTypography } from "unocss"
+import presetIcons from "@unocss/preset-icons"
+import compress from "astro-compress"
+
+// Icons
+import sections from "./src/data/sections.json" assert {type: "json"}
+import technologies from "./src/data/technologies.json" assert {type: "json"}
+import awards from "./src/data/awards.json" assert {type: "json"}
+import links from "./src/data/links.json" assert {type: "json"}
+import languages from "./src/data/languages.json" assert {type: "json"}
+import interests from "./src/data/interests.json" assert {type: "json"}
 
 // https://astro.build/config
 export default defineConfig({
   experimental: { integrations: true },
-  integrations: [tailwind(), compress()],
+  integrations: [
+    compress()
+  ],
   vite: {
-    ssr: {
-      external: ["svgo"],
-    },
+    plugins: [
+      unocss({
+        safelist: [
+          ...Array.from(sections, sec => `i-${sec.icon}`),
+          ...Array.from(technologies, tech => `i-${tech.icon}`),
+          ...Array.from(awards, aw => `i-${aw.icon}`),
+          ...Array.from(links, link => `i-${link.icon}`),
+          ...Array.from(languages, lang => `i-${lang.icon}`),
+          ...Array.from(interests, int => `i-${int.icon}`)
+        ],
+        presets: [
+          presetAttributify(),
+          presetUno(),
+          presetTypography(),
+          presetIcons({
+            autoInstall: true
+          })
+        ],
+      }),
+    ]
   },
-});
+})
