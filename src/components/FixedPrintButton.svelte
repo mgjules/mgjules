2f968ed1f288e3ecd24c80a63495598357fcef71
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-07-24 00:22:25 +0400

    better printer logo

--- src/components/FixedPrintButton.svelte
+++ src/components/FixedPrintButton.svelte
@@ -12,13 +12,8 @@
     on:click={print}
     type="button"
     aria-label="Print Resume"
-    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white transition-all border border-sky-900 rounded-md border-b-3 hover:border-b-2 opacity-75 bg-sky-600 group print:hidden hover:opacity-90 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white transition-all border border-sky-900 rounded-md border-b-3 hover:border-b-2 opacity-75 bg-sky-600 print:hidden hover:opacity-90 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
   >
-    <span class="hidden transition-all group-hover:inline-block"
-      >Print Resume</span
-    >
-    <div
-      class="i-ic:baseline-local-printshop bg-[length:100%_100%] w-6 h-6 group-hover:ml-2"
-    />
+    <div class="i-ic:baseline-local-printshop bg-[length:100%_100%] w-6 h-6" />
   </button>
 </div>
