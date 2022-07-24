1d018476114a9954f830c96ba861b694f9c37654
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-07-24 23:01:40 +0400

    headwind 🍃

--- src/components/FixedPrintButton.svelte
+++ src/components/FixedPrintButton.svelte
@@ -12,7 +12,7 @@
     on:click={print}
     type="button"
     aria-label="Print Resume"
-    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white transition-all border border-sky-900 rounded-md border-b-3 hover:border-b-2 opacity-75 bg-sky-600 print:hidden hover:opacity-90 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+    class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white transition-all border rounded-md opacity-75 border-sky-900 border-b-3 hover:border-b-2 bg-sky-600 print:hidden hover:opacity-90 hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
   >
     <div class="i-ic:baseline-local-printshop bg-[length:100%_100%] w-6 h-6" />
   </button>
