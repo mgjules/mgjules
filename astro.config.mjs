b34a62cd77f837a76970b3fda2b61c9b8a4e1f7c
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-07-29 01:07:10 +0400

    fix styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,9 +40,6 @@ export default defineConfig({
             'a:hover': {
               color: '#22d3ee',
             },
-            'a:visited': {
-              color: '#0e7490',
-            },
           },
         }),
         presetIcons({
