cc99f815a3ae5b1cf7c7da52aa948337598fc7ea
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-08-08 00:50:16 +0400

    use introduction api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import directus from "../utils/directus-client"
-import { Link } from "../utils/types"
+import { Link, Introduction } from "../utils/types"
 
 export async function getLinks(): Promise<Link[]> {
   const raw = await directus.items('links').readByQuery({
@@ -13,4 +13,8 @@ export async function getLinks(): Promise<Link[]> {
   });
 
   return raw.data as Link[] || []
+}
+
+export async function getIntroduction(): Promise<Introduction> {
+  return await directus.items('introduction').readOne("01a48809-19e1-4e23-9f91-d6182ddc28bf") as Introduction
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,8 @@ export interface Link {
   alternate_url?: string;
   new_window?: boolean;
 }
+
+export interface Introduction {
+  introduction: string;
+  avatar: string;
+}
