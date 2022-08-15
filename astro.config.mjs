1355019a03f674f4144cb1128a188319ee538ae9
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-08-15 12:05:57 +0400

    headers follow theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,10 +35,10 @@ export default defineConfig({
           brand: {
             foreground: '#252D38',
             background: '#191F28',
-            primary: theme.colors.gray['300'],
-            secondary: theme.colors.gray['500'],
-            tertiary: theme.colors.gray['600'],
-            accent: theme.colors.cyan['500']
+            primary: theme.colors.gray[300],
+            secondary: theme.colors.gray[500],
+            tertiary: theme.colors.gray[600],
+            accent: theme.colors.cyan[500]
           }
         }
       },
@@ -46,11 +46,14 @@ export default defineConfig({
         presetUno(),
         presetTypography({
           cssExtend: {
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme.colors.gray[300],
+            },
             'a,code': {
-              color: theme.colors.cyan['500']
+              color: theme.colors.cyan[500]
             },
             'a:hover': {
-              color: theme.colors.cyan['400']
+              color: theme.colors.cyan[400]
             }
           }
         }),
